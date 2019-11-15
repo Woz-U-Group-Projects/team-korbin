220f871a80a3692f3101c33f588dba848a140748
Owen Buckley <obuckleyjr@gmail.com>
2019-11-14 21:15:16 -0500

    Compiled front and back into same folder

--- routes/index.js
+++ routes/index.js
@@ -1,80 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-// HOME PAGE functions
+/* GET home page */
 router.get('/', function(req, res, next) {
-  res.render('get index', { title: 'Flavorly' });
-});
-
-router.post('/', function(req, res, next) {
-  res.send('post index');
-});
-
-router.put('/', function(req, res, next) {
-  console.log(req.params.id);
-  console.log(req.body.name);
-  res.send('put index');
-});
-
-router.delete('/', function(req, res, next) {
-  res.send('delete index');
-});
-
-// FULL FORM page functions
-router.get('/tasks', function(req, res, next) {
-  res.render('get tasks');
-});
-
-router.post('/', function(req, res, next) {
-  res.send(' post tasks');
-});
-
-router.put('/', function(req, res, next) {
-  console.log(req.params.id);
-  console.log(req.body.name);
-  res.send(' put tasks');
-});
-
-router.delete('/', function(req, res, next) {
-  res.send('delete tasks');
-});
-
-// SIMPLE FORM page functions
-router.get('/tasks', function(req, res, next) {
-  res.render('get tasks');
-});
-
-router.post('/', function(req, res, next) {
-  res.send(' post tasks');
-});
-
-router.put('/', function(req, res, next) {
-  console.log(req.params.id);
-  console.log(req.body.name);
-  res.send(' put tasks');
-});
-
-router.delete('/', function(req, res, next) {
-  res.send('delete tasks');
-});
-
-// RECIPE page functions
-router.get('/tasks', function(req, res, next) {
-  res.render('get tasks');
-});
-
-router.post('/', function(req, res, next) {
-  res.send(' post tasks');
-});
-
-router.put('/', function(req, res, next) {
-  console.log(req.params.id);
-  console.log(req.body.name);
-  res.send(' put tasks');
-});
-
-router.delete('/', function(req, res, next) {
-  res.send('delete tasks');
+  res.render('index', { title: 'Express'});
 });
 
 module.exports = router;
