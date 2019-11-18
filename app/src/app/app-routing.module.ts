a693eb9fb9c7b08a46eee5709335f9712485a3f5
Stephanie L Koukas <skoukas@gmail.com>
2019-11-17 21:46:10 -0500

    Moved routing to app-modules

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -3,11 +3,50 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RecipeDisplayComponent } from './recipe-display/recipe-display.component';
 
-
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent},
   { path: 'recipe-display', component: RecipeDisplayComponent},
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+
+  { path: 'create', component: DashboardComponent},
+  { path: 'edit/:id', component: DashboardComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+
+  //{ path: 'create', component: HomeComponent},
+  //{ path: 'edit/:id', component: HomeComponent},
+  //{ path: 'home', component: HomeComponent},
+  //{ path: '', redirectTo: 'home', pathMatch: 'full'},
+
+  // { path: 'create', component: RecipeHomeComponent},
+  // { path: 'edit/:id', component: RecipeHomeComponent},
+  // { path: 'recipehome', component: RecipeHomeComponent},
+  // { path: '', redirectTo: 'recipehome', pathMatch: 'full'},
+
+  { path: 'create', component: RecipeDisplayComponent},
+  { path: 'edit/:id', component: RecipeDisplayComponent},
+  { path: 'recipedisplay', component: RecipeDisplayComponent},
+  { path: '', redirectTo: 'reciedisplay', pathMatch: 'full'}
+
+  // { path: 'create', component: QuickAddComponent},
+  // { path: 'edit/:id', component: QuickAddComponent},
+  // { path: 'quickadd', component: QuickAddComponent},
+  // { path: '', redirectTo: 'quickadd', pathMatch: 'full'},
+
+  // { path: 'create', component: TaskDisplayComponent},
+  // { path: 'edit/:id', component: TaskDisplayComponent},
+  // { path: 'taskdisplay', component: TaskDisplayComponent},
+  // { path: '', redirectTo: 'taskdisplay', pathMatch: 'full'},
+
+  // { path: 'create', component: NavBarComponent},
+  // { path: 'edit/:id', component: NavBarComponent},
+  // { path: 'navbar', component: NavBarComponent},
+  // { path: '', redirectTo: 'navbar', pathMatch: 'full'},
+
+  // { path: 'create', component: AppComponent},
+  // { path: 'edit/:id', component: AppComponent},
+  // { path: 'app', component: AppComponent},
+  // { path: '', redirectTo: 'app', pathMatch: 'full'},
 ];
 
 @NgModule({
