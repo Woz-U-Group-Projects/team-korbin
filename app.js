8cc058fa335300725f904990e111648096c9d9cc
Owen Buckley <obuckleyjr@gmail.com>
2019-11-21 18:41:28 -0500

    Initiated integration of back to front

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-//var cors = require("cors");
+var cors = require("cors");
 // var models = require("./models");
 
 //pulling in routes
@@ -19,6 +19,13 @@ var app = express();
 //view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+//CORS
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(logger("dev"));
 app.use(express.json());
