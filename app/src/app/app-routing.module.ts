a2a532415cb7463ae1a8a203f4c2e3ccd309cab5
Stephanie L Koukas <skoukas@gmail.com>
2019-12-01 11:46:26 -0500

    Added full-form and updated app-routing

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -6,36 +6,24 @@ import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { QuickAddComponent } from './quick-add/quick-add.component';
 import { RecipeDisplayComponent } from './recipe-display/recipe-display.component';
-<<<<<<< HEAD
-=======
-import { QuickAddService } from './services/quick-add.service';
->>>>>>> bf1708d2232b9753e031b6be961a76191dcbff51
 import { RecipeHomeComponent } from './recipe-home/recipe-home.component';
+import { RecipeImageCardComponent } from './recipe-image-card/recipe-image-card.component';
+import { FullFormComponent } from './full-form/full-form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'recipedisplay', pathMatch: 'full'},
   { path: 'dashboard', component: DashboardComponent},
-<<<<<<< HEAD
+  { path: 'full-form', component: FullFormComponent},
   { path: 'home', component: HomeComponent},
   { path: 'nav-bar', component: NavBarComponent},
   { path: 'quick-add', component: QuickAddComponent},
   { path: 'recipe-display', component: RecipeDisplayComponent},
-  { path: 'recipe-home', component: RecipeHomeComponent }
+  { path: 'recipe-home', component: RecipeHomeComponent },
+  { path: 'recipe-image-card', component: RecipeImageCardComponent},
   // { path: 'recipe/:id', component: RecipeDisplayComponent},
   // { path: 'edit/:id', component: RecipeDisplayComponent},
   // { path: 'recipedisplay', component: RecipeDisplayComponent},
-=======
-  { path: 'newentry', component: QuickAddService},
-  { path: 'recipe-display', component: RecipeDisplayComponent},
-  { path: 'recipe/:id', component: RecipeDisplayComponent},
-  { path: 'recipe-home', component: RecipeHomeComponent},
-  // { path: 'update/:id', component: },  //This will be the path to update a recipe
-  // { path: 'recipedisplay', component: RecipeDisplayComponent},  //**delete */
-  // { path: '', redirectTo: 'recipedisplay', pathMatch: 'full'}  //**delete */
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
->>>>>>> bf1708d2232b9753e031b6be961a76191dcbff51
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ app/src/app/services/full-form.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Recipe } from "../models/recipe";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root"
+})
+export class FullFormService {
+  // Java Spring Boot uses port 8080
+  // apiUrl: string = "http://localhost:8080/quickadds";
+
+  // C# dotnetcore uses port 5000
+  //apiUrl: string = "http://localhost:5000/api/quickadds";
+
+  // Express will use port 3000
+  apiUrl: string = "http://localhost:3000/recipes";
+
+  constructor(private http: HttpClient) {}
+
+  getRecipes(): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>(this.apiUrl);
+  }
+
+  addRecipe(quickadd): Observable<Recipe> {
+    return this.http.post<Recipe>(`${this.apiUrl}/newentry`, FullFormService);
+  }
+}
