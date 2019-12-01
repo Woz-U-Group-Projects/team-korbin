546c29fcebc3c253886383132530c8c295174d8b
Stephanie L Koukas <skoukas@gmail.com>
2019-12-01 10:45:08 -0500

    Updated the backend delete routing code

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { QuickAddComponent } from './quick-add/quick-add.component';
 import { RecipeDisplayComponent } from './recipe-display/recipe-display.component';
+import { RecipeHomeComponent } from './recipe-home/recipe-home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'recipedisplay', pathMatch: 'full'},
   { path: 'dashboard', component: DashboardComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'nav-bar', component: NavBarComponent},
+  { path: 'quick-add', component: QuickAddComponent},
   { path: 'recipe-display', component: RecipeDisplayComponent},
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-
-  { path: 'recipe/:id', component: RecipeDisplayComponent},
-  { path: 'edit/:id', component: RecipeDisplayComponent},
-  { path: 'recipedisplay', component: RecipeDisplayComponent},
-  { path: '', redirectTo: 'recipedisplay', pathMatch: 'full'}
+  { path: 'recipe-home', component: RecipeHomeComponent }
+  // { path: 'recipe/:id', component: RecipeDisplayComponent},
+  // { path: 'edit/:id', component: RecipeDisplayComponent},
+  // { path: 'recipedisplay', component: RecipeDisplayComponent},
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- routes/recipes.js
+++ routes/recipes.js
@@ -7,6 +7,7 @@ router.get("/", function(req, res, next) {
   Recipe.find().then(recipes => res.json(recipes));
 });
 
+//POST  new entry from dashboard
 router.post("/newentry", function(req, res, next) {
   let newRecipe = new Recipe();
   newRecipe.save(
@@ -19,15 +20,39 @@ router.post("/newentry", function(req, res, next) {
     ],
     newRecipe.image = req.body.ingredientImage,
     newRecipe.category = req.body.category,
+    newRecipe.directiona = req.body.directions,
     newRecipe.new = true
     )
     .then(recipe => 
       res.json(recipe))
     .catch(err => {
-      res.status(400).send("Unable to save data to database on POST.  Error: " + err );
+      res.status(400).send("Unable to save data to database on POST.  Error: " + err )
     });
 });
 
+//POST new entry from sidebar
+router.post("/newentry/sidebar", function(req, res, next) {
+  let newRecipe = new Recipe();
+  newRecipe.save(
+    newRecipe.name = req.body.recipeName,
+    newRecipe.ingredients = req.body.ingredients[
+      newRecipe.ingredientItem = req.body.ingredientItem,
+      newRecipe.ingredientQty = req.body.ingredientQty,
+      newRecipe.ingredientMeasurement = req.body.ingredientMeasurement
+    ],
+    newRecipe.image = req.body.ingredientImage,
+    newRecipe.category = req.body.category,
+    newRecipe.directiona = req.body.directions,
+    newRecipe.new = true
+    )
+    .then(recipe => 
+      res.json(recipe))
+    .catch(err => {
+      res.status(400).send("Unable to save data to database on POST.  Error: " + err )
+    });
+});
+
+//PUT Update from new entry from dashboard
 router.put("/:rId", function(req, res) {
   RecipeModel.findByIdAndUpdate(
     req.params.rId,
@@ -37,22 +62,63 @@ router.put("/:rId", function(req, res) {
     ingredientQty: req.body.ingredientQty,
     ingredientMeasurement: req.body.ingredientMeasurement,
     image: req.body.ingredientImage,
-    category: req.body.category},
-    { new: true },
-    (err, recipe) => {
-      if (err) return res.status(400).send(err);
-      res.send(recipe);
+    category: req.body.category
     }
-  );
+    .then(recipe => 
+      res.json(recipe))
+    .catch(err => {
+      res.status(400).send("Unable to save data to database on POST.  Error: " + err )
+  }));
 });
 
+//PUT Update from new entry from sidebar
+router.put("/:rId", function(req, res) {
+  RecipeModel.findByIdAndUpdate(
+    req.params.rId,
+    {name: req.body.recipeName,
+    ingredients: req.body.ingredients,
+    ingredientItem: req.body.ingredientItem,
+    ingredientQty: req.body.ingredientQty,
+    ingredientMeasurement: req.body.ingredientMeasurement,
+    image: req.body.ingredientImage,
+    category: req.body.category
+    }
+    .then(recipe => 
+      res.json(recipe))
+    .catch(err => {
+      res.status(400).send("Unable to save data to database on sidebar POST.  Error: " + err )
+  }));
+});
+
+//PUT Update from new entry from dashbord
 router.delete("/:rId", function(req, res, next) {
-  RecipeModel.findByIdAndRemove(req.params.rId, (err, recipe) => {
-    if (err) return res.status(400).send(err);
-    res.send(recipe);
-  });
+  Model.RecipeModel.findByIdAndRemove(
+    req.params.rId, (error, data) => {
+        if (error) {
+		    return next (error);
+		    } else {
+		    res.status(200).json
+        Mag: data
+    .then(recipe => 
+      res.json(recipe))
+    .catch(err => {
+      res.status(400).send("Unable to save data to database on POST.  Error: " + err )
+    }
+  )};
+  })
 });
 
+// recipesRoute.route(‘/delete/:rId’).delete{(req, res, next) => {
+// 	Recipes.findOneAndRemove(req.params.id, (error, data) => {
+// 		If (error) {
+// 		return next (error);
+// 		} else {
+// 		res.status(200).json
+//     Mag: data
+//     })
+// 		}
+// 	})
+// })
 
 module.exports = router;
 // module.exports = Recipe;
