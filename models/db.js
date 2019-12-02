c80fedbd30770c26bf54e768e9228dbca46f3c8e
LaRojaFuria <escarmical@gmail.com>
2019-12-01 15:30:48 -1000

    Database connection

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = requires('mongoose');
 
-mongoose.connect('mongodb://localhost:3000/recipeapp-db', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb+srv://team-korbin:RECIPEAPP@cluster0-rsteg.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeesed.') }
     else {console.log('Error in DB connection : ' + err) }
 });
