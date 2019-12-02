36da8cccb689d610becd48871040db98748b5bd4
LaRojaFuria <escarmical@gmail.com>
2019-12-01 15:47:16 -1000

    Database coonection

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(function(err, req, res, next) {
 
 //var mongoDB = "mongodb://127.0.0.1/database";
 var mongoDB =
-  "mongodb+srv://ammon:Password1%21@cluster0-lhvh5.mongodb.net/test?retryWrites=true&w=majority";
+  "mongodb+srv://team-korbin:RECIPEAPP@cluster0-rsteg.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
