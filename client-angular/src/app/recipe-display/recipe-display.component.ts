af1abd3a283ea22747ca46b7aba33d7fb5145631
Owen Buckley <obuckleyjr@gmail.com>
2019-12-06 08:34:32 -0500

    Build out of recipe dashboard

--- client-angular/src/app/recipe-display/recipe-display.component.ts
+++ client-angular/src/app/recipe-display/recipe-display.component.ts
@@ -8,25 +8,26 @@ import { Recipe } from "../models/recipe";
   styleUrls: ["./recipe-display.component.css"]
 })
 export class RecipeDisplayComponent implements OnInit {
-  constructor(private recipeService: RecipeService) {}
+  constructor(private recipeService: RecipeService) { }
   newRecipe: Recipe = new Recipe();
   recipes: Recipe[] = [];
 
-  
+
   getRecipes() {
     this.recipeService.getRecipes().subscribe(recipes => (this.recipes = recipes));
   }
-  
+
   addRecipe() {
     const recipeName = this.newRecipe.recipeName;
     const id = this.newRecipe.id;
+    const category = this.newRecipe.category;
     id + 1;
 
     this.recipeService.addRecipe(this.newRecipe).subscribe(newRecipe => {
       newRecipe = new Recipe();
       newRecipe.id = id;
       newRecipe.recipeName = recipeName;
-      // newRecipe.category = category;
+      newRecipe.category = category;
       newRecipe.complete = true;
       this.getRecipes();
     });
